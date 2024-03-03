351f3c5866eef910fc9b00315483be391de529b7
Kuray Karaaslan <kuraykaraaslan@gmail.com>
2024-03-04 00:02:54 +0300

    Starting

---
+++ app/_layout.tsx
@@ -0,0 +1,11 @@
+import { Stack } from 'expo-router';
+import { NativeWindStyleSheet } from 'nativewind';
+
+
+NativeWindStyleSheet.setOutput({
+  default: 'native',
+});
+
+export default function Layout() {
+  return <Stack />;
+}
